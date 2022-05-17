440b3a09dba49dc2222271575a758be61e4a91d8
Juz Bird <daweiwizard@gmail.com>
2022-05-17 21:21:55 +0630

    add map location

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,4 +1,3 @@
-import img from "../Img/location.png";
 
 const Contact = () => {
     return (
@@ -7,7 +6,7 @@ const Contact = () => {
             <h3>MAPLE MONTESSORI PRESCHOOL</h3>
             <h4>SCOUT HALL, WILSON ST, WINCHMORE HILL, LONDON N21 1BP</h4>
 
-            <img src={img} className="location" />
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2476.3226750534295!2d-0.10211774420178983!3d51.6356155!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x487619887d9c6401%3A0xf05714aae17b836b!2sMaple%20Montessori%20Preschool!5e0!3m2!1sen!2smm!4v1652798403039!5m2!1sen!2smm" width="100%" height="450" className="border-less" title="Pre school"></iframe>
         </div>
     )
 }
